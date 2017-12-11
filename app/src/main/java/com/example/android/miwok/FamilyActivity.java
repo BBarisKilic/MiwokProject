1699f1287818c3743d0bd85d6d95e8e3ecc57116
eopeace <r>
2017-12-11 03:25:00 +0300

    Supporting activity lifecycle and Added AudioManager.

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -1,5 +1,7 @@
 package com.example.android.miwok;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,6 +16,31 @@ public class FamilyActivity extends AppCompatActivity {
     //global mediaplayer hem itemclick dinlerken hemnde hafıza boşaltırken çağirmak için.
     private MediaPlayer mediaPlayer;
 
+    //bu bizim sesimizin arama geldiğinde,mesaj geldiğinde nasıl davranması gerektiğini ayarlamak için yarat-
+    //tığımız global veri.
+    private AudioManager audioManager;
+
+    AudioManager.OnAudioFocusChangeListener onAudioFocusChangeListener =
+            new AudioManager.OnAudioFocusChangeListener() {
+                @Override
+                public void onAudioFocusChange(int focusChange) {
+                    //bu durumlarda çalmayı durdur.(pause)
+                    if(focusChange==AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ||
+                            focusChange==AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK){
+                        mediaPlayer.pause();
+                        mediaPlayer.seekTo(0);
+
+                        //bu durumda çalmaya devam et.
+                    }else if (focusChange== AudioManager.AUDIOFOCUS_GAIN) {
+                        mediaPlayer.start();
+
+                        //çalmayı tamamen durdur.(stop)
+                    }else if (focusChange==AudioManager.AUDIOFOCUS_LOSS){
+                        releaseMediaSources();
+                    }
+                }
+            };
+
     //kaynak boşaltıcak fonksiyonu çağirmak için sesin bitmesini bekleyen method.
     private MediaPlayer.OnCompletionListener mcompletionListener = new MediaPlayer.OnCompletionListener(){
         @Override
@@ -26,6 +53,9 @@ public class FamilyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        //audio focus yapmamızı sağlayacak şekilde servisi çağırıyoruz.
+        audioManager =(AudioManager) getSystemService(Context.AUDIO_SERVICE);
 
         final ArrayList<Word> family = new ArrayList<Word>();
 
@@ -53,15 +83,33 @@ public class FamilyActivity extends AppCompatActivity {
                 Word word = family.get(position);
                 //kullanıcı hızlıca sekmelere basiyorsa önceki sesi boşaltiyor böylece kaynak açılıyor setoncompletion beklenmeden.
                 releaseMediaSources();
-                //yeni kaynak çağiriliyor, oluşturuluyor.
-                mediaPlayer=MediaPlayer.create(FamilyActivity.this, word.getMusicID());
-                //ses çalınıyor.
-                mediaPlayer.start();
-                //ses çalması bitince hafızadan atiliyor. yukarıda global olarak tanımlandı method...böylece her seferinde oluşturulup boşuna hafıza yenilmeyecek.
-                mediaPlayer.setOnCompletionListener(mcompletionListener);
+
+                //çalmak için audiofocus iste
+                int result = audioManager.requestAudioFocus(onAudioFocusChangeListener,
+                        //müzik streami kullan
+                        AudioManager.STREAM_MUSIC,
+                        //tam focus iste.
+                        AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+                //audiofocus hazır şuan(yani hazırsa kod çalışabilir)
+                if(result==AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    //yeni kaynak çağiriliyor, oluşturuluyor.
+                    mediaPlayer = MediaPlayer.create(FamilyActivity.this, word.getMusicID());
+                    //ses çalınıyor.
+                    mediaPlayer.start();
+                    //ses çalması bitince hafızadan atiliyor. yukarıda global olarak tanımlandı method...böylece her seferinde oluşturulup boşuna hafıza yenilmeyecek.
+                    mediaPlayer.setOnCompletionListener(mcompletionListener);
+                }
             }
         });
 
+    }
+
+    //onStop lifecycle parçalarından biri, eğer kullanıcı uygulamayı ana ekran tuşuna basıp başka bir uygulamaya geçerse yada ana menüye dönerse çalışır.
+    @Override
+    protected void onStop() {
+        super.onStop();
+        releaseMediaSources();
     }
 
     //hafızadan ses kaynağini boşaltan fonksiyon
@@ -69,6 +117,7 @@ public class FamilyActivity extends AppCompatActivity {
         if(mediaPlayer!=null){
             mediaPlayer.release();
             mediaPlayer=null;
+            audioManager.abandonAudioFocus(onAudioFocusChangeListener);
         }
     }
 }
--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,5 +1,7 @@
 package com.example.android.miwok;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,6 +17,33 @@ public class NumbersActivity extends AppCompatActivity {
     //global mediaplayer hem itemclick dinlerken hemnde hafıza boşaltırken çağirmak için.
     private MediaPlayer mediaPlayer;
 
+    //bu bizim sesimizin arama geldiğinde,mesaj geldiğinde nasıl davranması gerektiğini ayarlamak için yarat-
+    //tığımız global veri.
+    private AudioManager audioManager;
+
+    AudioManager.OnAudioFocusChangeListener onAudioFocusChangeListener =
+            new AudioManager.OnAudioFocusChangeListener() {
+                @Override
+                public void onAudioFocusChange(int focusChange) {
+                    //bu durumlarda çalmayı durdur.(pause)
+                    if(focusChange==AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ||
+                            focusChange==AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK){
+                        mediaPlayer.pause();
+                        mediaPlayer.seekTo(0);
+
+                        //bu durumda çalmaya devam et.
+                    }else if (focusChange== AudioManager.AUDIOFOCUS_GAIN) {
+                        mediaPlayer.start();
+
+                        //çalmayı tamamen durdur.(stop)
+                    }else if (focusChange==AudioManager.AUDIOFOCUS_LOSS){
+                        releaseMediaSources();
+                    }
+                }
+            };
+
+
+
     //kaynak boşaltıcak fonksiyonu çağirmak için sesin bitmesini bekleyen method.
     private MediaPlayer.OnCompletionListener mcompletionListener = new MediaPlayer.OnCompletionListener(){
         @Override
@@ -28,6 +57,8 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
+        //audio focus yapmamızı sağlayacak şekilde servisi çağırıyoruz.
+        audioManager =(AudioManager) getSystemService(Context.AUDIO_SERVICE);
 
         final ArrayList<Word> words = new ArrayList<Word>();
 
@@ -55,14 +86,32 @@ public class NumbersActivity extends AppCompatActivity {
                 Word word = words.get(position);
                 //kullanıcı hızlıca sekmelere basiyorsa önceki sesi boşaltiyor böylece kaynak açılıyor setoncompletion beklenmeden.
                 releaseMediaSources();
-                //yeni kaynak çağiriliyor, oluşturuluyor.
-                mediaPlayer=MediaPlayer.create(NumbersActivity.this, word.getMusicID());
-                //ses çalınıyor.
-                mediaPlayer.start();
-                //ses çalması bitince hafızadan atiliyor. yukarıda global olarak tanımlandı method...böylece her seferinde oluşturulup boşuna hafıza yenilmeyecek.
-                mediaPlayer.setOnCompletionListener(mcompletionListener);
+
+                //çalmak için audiofocus iste
+                int result = audioManager.requestAudioFocus(onAudioFocusChangeListener,
+                        //müzik streami kullan
+                        AudioManager.STREAM_MUSIC,
+                        //tam focus iste.
+                        AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+                //audiofocus hazır şuan(yani hazırsa kod çalışabilir)
+                if(result==AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    //yeni kaynak çağiriliyor, oluşturuluyor.
+                    mediaPlayer = MediaPlayer.create(NumbersActivity.this, word.getMusicID());
+                    //ses çalınıyor.
+                    mediaPlayer.start();
+                    //ses çalması bitince hafızadan atiliyor. yukarıda global olarak tanımlandı method...böylece her seferinde oluşturulup boşuna hafıza yenilmeyecek.
+                    mediaPlayer.setOnCompletionListener(mcompletionListener);
+                }
             }
         });
+    }
+
+    //onStop lifecycle parçalarından biri, eğer kullanıcı uygulamayı ana ekran tuşuna basıp başka bir uygulamaya geçerse yada ana menüye dönerse çalışır.
+    @Override
+    protected void onStop() {
+        super.onStop();
+        releaseMediaSources();
     }
 
     //hafızadan ses kaynağini boşaltan fonksiyon
@@ -70,6 +119,7 @@ public class NumbersActivity extends AppCompatActivity {
         if(mediaPlayer!=null){
             mediaPlayer.release();
             mediaPlayer=null;
+            audioManager.abandonAudioFocus(onAudioFocusChangeListener);
         }
     }
 }
